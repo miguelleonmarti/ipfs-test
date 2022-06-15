69e35524ce30bab66f20b6c8e976fd628f72b73b
miguelleonmarti <miguelleonmarti@gmail.com>
2022-06-15 14:21:45 +0100

    feature: connect to wallet

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,29 @@
-import Head from "next/head";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>IPFS storage</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+  const [address, setAddress] = useState(null);
+  const [element, setElement] = useState(null);
 
-      <main>It works!</main>
-    </div>
-  );
+  useEffect(() => {
+    if (typeof window !== "undefined") setElement(window);
+  }, []);
+
+  async function connectWallet() {
+    try {
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const address = await signer.getAddress();
+      setAddress(address);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  if (!element?.ethereum) {
+    return <div>Please install an ethereum wallet</div>;
+  }
+
+  return <div>{address ? <div>{address}</div> : <button onClick={connectWallet}>Connect</button>}</div>;
 }
