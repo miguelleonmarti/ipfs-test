7e3aaeb6fcfb48ae15a6cd863afb091fdfa28279
miguelleonmarti <miguelleonmarti@gmail.com>
2022-06-15 13:08:10 +0100

    chore: client (nextjs) setup

---
+++ client/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>IPFS storage</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>It works!</main>
+    </div>
+  );
+}
